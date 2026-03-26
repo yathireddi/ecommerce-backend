05707ef70bfe7a816566985c9c891eb2459b1613
yathireddi <2400031770@kluniversity.in>
2026-03-26 10:28:50 +0530

    Update SecurityConfig.java

--- src/main/java/com/klu/config/SecurityConfig.java
+++ src/main/java/com/klu/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-            "https://ecommerce-frontend-production-2779.up.railway.app"
+            "ecommerce-backend-production-0489.up.railway.app"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
